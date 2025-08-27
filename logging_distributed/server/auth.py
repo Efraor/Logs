cecd2a52de093bb9f18bdda47895629ac29602ce
Efrain Ortiz <efrainsis74@gmail.com>
2025-08-27 15:31:05 -0300

    Agrego la autorizacion del token

--- logging_distributed/server/auth.py
+++ logging_distributed/server/auth.py
@@ -1,1 +1,11 @@
 # Funciones de autenticación y validación de tokens
+
+from flask import request, jsonify
+
+API_TOKEN = "MiTokenSecreto123"
+
+def validate_token():
+    token = request.headers.get("Authorization", "")
+    if not token or token.replace("Bearer ", "") != API_TOKEN:
+        return jsonify({"error": "Unauthorized"}), 401
+    return None
